7604b04b0082756c45148e81ce4ed0562963b9b6
Abzal Tokhirov <abzal_tokhirov@epam.com>
2023-11-22 22:18:14 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+
+
+[AttributeUsage(AttributeTargets.Property)]
+public class ConfigurationItemAttribute : Attribute
+{
+    public string SettingName { get; }
+    public string ProviderType { get; }
+
+    public ConfigurationItemAttribute(string settingName, string providerType)
+    {
+        SettingName = settingName;
+        ProviderType = providerType;
+    }
+}
+
+
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
